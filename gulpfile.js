46167a1d42c992d81084c0c876fe382493885781
lillianchow <lillianchow8@gmail.com>
2018-01-15 19:06:58 -0500

    picture update
    update picture on homepage

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,21 @@ var banner = ['/*!\n',
   ''
 ].join('');
 
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  });
+  
 // Compiles SCSS files from /scss into /css
 gulp.task('sass', function() {
   return gulp.src('scss/agency.scss')
